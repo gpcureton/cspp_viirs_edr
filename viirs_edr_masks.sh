d7ea2e6b1ce26a47fd60369c7380aded62d79b48
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2013-01-16 18:45:35 +0000

    Tweaked to properly handle usage and help_usage options.
    git-svn-id: https://svn.ssec.wisc.edu/repos/jpss_adl/trunk/scripts/edr@1142 145b5396-d1c6-4523-8c67-3646d8218c8b

--- viirs_edr_masks.sh
+++ viirs_edr_masks.sh
@@ -47,6 +47,7 @@ Run the ADL VIIRS EDR.
 
 Usage: 
     export CSPP_HOME=/path/to/CSPP/dir
+    source \$CSPP_HOME/cspp_env.sh
     \$CSPP_HOME/viirs/edr/viirs_edr_masks.sh [mandatory args] [options]
 
 
@@ -60,12 +61,13 @@ Options:
     At a minimum these arguments must be specified
 
     -i INPUTFILES, --input_files=INPUTFILES
-                        The base directory where input are placed
+                        The fully qualified path to the input files. 
+                        May be a directory or a file glob.
     
   Extra Options:
     These options may be used to customize behaviour of this program.
 
-    -w WORK_DIR, --workDirectory=WORK_DIR
+    -w WORK_DIR, --work_directory=WORK_DIR
                         The directory which all activity will occur in,
                         defaults to the current directory.
 
@@ -82,7 +84,7 @@ Options:
     --sdr_endianness=SDR_ENDIANNESS
                         The input VIIRS SDR endianness.
                         Possible values are...
-                        'little', 'big'
+                        'little', 'big'. [default: 'little']
 
     --anc_endianness=ANC_ENDIANNESS
                         The input VIIRS ancillary endianness.
@@ -102,6 +104,7 @@ Run the ADL VIIRS EDR.
 
 Usage: 
     export CSPP_HOME=/path/to/CSPP/dir
+    source \$CSPP_HOME/cspp_env.sh
     \$CSPP_HOME/viirs/edr/viirs_edr_masks.sh [mandatory args] [options]
 
   -h, --help            Show the mandatory args and options and exit.
@@ -139,61 +142,102 @@ fi
 eval set -- "$OPTS"
 
 # Now go through all the options
+haveFlag=0
+helpFlag=0
+usageFlag=0
+
 while true ;
 do
-    #echo "Checking opts"
     case "$1" in
         -i|--input_files)
             INPUT_FILES_OPT="--input_files=$2"
+            #echo "Setting INPUT_FILES_OPT"
+            haveFlag=1
             shift 2;;
 
         -w|--work_directory)
             WORK_DIR_OPT="--work_directory=$2"
+            #echo "Setting WORK_DIR_OPT"
+            haveFlag=1
             shift 2;;
 
         --sdr_endianness)
             SDR_ENDIANNESS_OPT="--sdr_endianness=$2"
+            echo "Setting SDR_ENDIANNESS_OPT"
+            haveFlag=1
             shift 2;;
 
         --anc_endianness)
             ANC_ENDIANNESS_OPT="--anc_endianness=$2"
+            #echo "Setting ANC_ENDIANNESS_OPT"
+            haveFlag=1
             shift 2;;
 
         --skip_sdr_unpack)
             SKIP_SDR_UNPACK_OPT="--skip_sdr_unpack"
+            #echo "Setting SKIP_SDR_UNPACK_OPT"
+            haveFlag=1
             shift ;;
 
         --skip_aux_linking)
             SKIP_AUX_LINKING_OPT="--skip_aux_linking"
+            #echo "Setting SKIP_AUX_LINKING_OPT"
+            haveFlag=1
             shift ;;
 
         --skip_ancillary)
             SKIP_ANCILLARY_OPT="--skip_ancillary"
+            #echo "Setting SKIP_ANCILLARY_OPT"
+            haveFlag=1
             shift ;;
 
         --skip_algorithm)
             SKIP_ALGORITHM_OPT="--skip_algorithm"
+            #echo "Setting SKIP_ALGORITHM_OPT"
+            haveFlag=1
             shift ;;
 
         -d|--debug)
             DEBUG_OPT="--debug"
+            #echo "Setting DEBUG_OPT"
+            haveFlag=1
             shift ;;
 
         -v|--verbose)
             VERBOSITY_OPT="-"$(echo $VERBOSITY_OPT | sed s#-##)"v"
+            #echo "Setting VERBOSITY_OPT"
+            haveFlag=1
             shift ;;
 
         -h|--help)
-            help_usage
+            if [[ $haveFlag -eq 0 ]];
+            then
+                helpFlag=1
+            fi
             shift;
             break ;;
 
-        --) 
-            usage
+        --)
+            if [[ $haveFlag -eq 0 ]];
+            then
+                usageFlag=1
+            fi
             shift;
             break;;
+
     esac
 done
+
+if [[ $helpFlag -eq 1 ]];
+then
+    help_usage
+    exit 0
+fi
+if [[ $usageFlag -eq 1 ]];
+then
+    usage
+    exit 0
+fi
 
 
 echo "INPUT_FILES_OPT      = "$INPUT_FILES_OPT
