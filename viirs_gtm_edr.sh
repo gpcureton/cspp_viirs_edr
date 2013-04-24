71eec80e062d3336f4eb1e611b14e3f58512c589
Ray Garcia <rayg@ssec.wisc.edu>
2013-04-24 13:54:39 +0000

    baseline
    git-svn-id: https://svn.ssec.wisc.edu/repos/jpss_adl/trunk/scripts/edr@1300 145b5396-d1c6-4523-8c67-3646d8218c8b

--- viirs_gtm_edr.sh
+++ viirs_gtm_edr.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Wrapper script for VIIRS SDR components from ADL 3.1
 #
-# Copyright 2011, University of Wisconsin Regents.
+# Copyright 2013, University of Wisconsin Regents.
 # Licensed under the GNU GPLv3.
 
-if [ -z "$CSPP_SDR_HOME" ]; then
-  echo "CSPP_SDR_HOME must be set to the path where the CSPP software was installed."
-  echo "export CSPP_SDR_HOME=/home/me/SDR_x_x"
+if [ -z "$CSPP_EDR_HOME" ]; then
+  echo "CSPP_EDR_HOME must be set to the path where the CSPP software was installed."
+  echo "export CSPP_EDR_HOME=/home/me/EDR_x_x"
   exit 1
 fi
 
@@ -14,31 +14,31 @@ fi
 usage() {
   cat <<EOF
 Usage:
-  export CSPP_SDR_HOME=/path/to/here
-  \$CSPP_SDR_HOME/viirs/sdr/viirs_sdr.sh [OPTIONS] viirs-rdr-1.h5 [viirs-rdr-2.h5 [viirs-rdr-3.h5 [...]]]
+  export CSPP_EDR_HOME=/path/to/here
+  \$CSPP_EDR_HOME/viirs/sdr/viirs_gtm_edr.sh [OPTIONS] viirs-rdr-1.h5 [viirs-rdr-2.h5 [viirs-rdr-3.h5 [...]]]
 
    -h                      Print this message
    -d                      Optional. If specified, intermediate files will always be 
                           retained
-   -z                     Optional.  Compress with h5repack zip
+   -z                     Optional.  Compress with h5repack
    -a                     Optional.  Aggregate using nagg tool
    -l                     Optional.  Inhibit automatic download of ancillary files
 
   
-  One or more VIIRS RDR HDF5 files containing 1 or more  granules is needed. 
+  One or more VIIRS SDR HDF5 files containing 1 or more  granules is needed.
   Wildcards are allowed. Files can be specified in any order.
 
 Example:
-  \$CSPP_SDR_HOME/viirs/sdr/viirs_sdr.sh  /data/rdr/RVIIRS-RNSCA_npp_d20030125_t06*h5 
+  \$CSPP_EDR_HOME/viirs/sdr/viirs_gtm_edr.sh /data/sdr/SV*h5
 EOF
 
 }
 
-. ${CSPP_SDR_HOME}/common/cspp_common.sh
+. ${CSPP_EDR_HOME}/common/cspp_common.sh
 
 if [ -z "$1" ]; then
     usage
   exit 3
 fi
 
-$PY $CSPP_RT_HOME/viirs/adl_viirs_sdr.py -vv "$@"
+$PY $CSPP_RT_HOME/viirs/adl_viirs_gtm_edr.py -vv "$@"
