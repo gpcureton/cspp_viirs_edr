71751e80004bada536993f84923a923dced956b9
Scott Mindock <scott.mindock@ssec.wisc.edu>
2013-10-15 13:56:58 +0000

    Work with env variables.
    git-svn-id: https://svn.ssec.wisc.edu/repos/jpss_adl/trunk/scripts/edr@1735 145b5396-d1c6-4523-8c67-3646d8218c8b

--- viirs_gtm_edr.sh
+++ viirs_gtm_edr.sh
@@ -4,17 +4,17 @@
 # Copyright 2013, University of Wisconsin Regents.
 # Licensed under the GNU GPLv3.
 
-if [ -z "$CSPP_EDR_HOME" ]; then
-  echo "CSPP_EDR_HOME must be set to the path where the CSPP software was installed."
-  echo "export CSPP_EDR_HOME=/home/me/EDR_x_x"
+if [ -z "$CSPP_GTM_HOME" ]; then
+  echo "CSPP_GTM_HOME must be set to the path where the CSPP software was installed."
+  echo "export CSPP_GTM_HOME=/home/me/GTM_x_x"
   exit 1
 fi
 
 . ${CSPP_GTM_HOME}/common/cspp_common.sh
 
 if [ -z "$1" ]; then
-  $PY $CSPP_RT_HOME/viirs/adl_viirs_gtm_edr.py -h
+  $PY $CSPP_GTM_HOME/viirs/adl_viirs_gtm_edr.py -h
   exit 3
 fi
 
-$PY $CSPP_RT_HOME/viirs/adl_viirs_gtm_edr.py -vv "$@"
+$PY $CSPP_GTM_HOME/viirs/adl_viirs_gtm_edr.py -vv "$@"
