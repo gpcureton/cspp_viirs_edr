4d761e990b580983d0ddfe51544006e0f21a752f
Scott Mindock <scott.mindock@ssec.wisc.edu>
2014-06-18 20:48:46 +0000

    added unset scripts for use with complicated environments.
    git-svn-id: https://svn.ssec.wisc.edu/repos/jpss_adl/trunk/scripts/edr@2132 145b5396-d1c6-4523-8c67-3646d8218c8b

---
+++ cspp_edr_unset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+unset CSPP_RT_ANC_CACHE_DIR
+unset CSPP_RT_ANC_PATH
+unset CSPP_RT_ANC_HOME
+unset CSPP_RT_ANC_TILE_PATH
+unset CSPP_RT_HOME
+unset DCONFIG
+unset JPSS_REMOTE_ANC_DIR
+
